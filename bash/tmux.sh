8ff2a879ccad53ac387258ef47e25c043c0fc0b5
edwardlavender <edward.lavender@eawag.ch>
2025-04-29 17:01:03 +0200

    Add comments

--- bash/tmux.sh
+++ bash/tmux.sh
@@ -1,5 +1,10 @@
 # Deploy analysis-patter.R on siam-linux20
 
+# Instructions
+# * (optional) Clean up output folders, including logs/ and runs/
+# * Customise arguments below as required
+# * Run tmux code below to deploy script
+
 # tmux cheatsheet:
 # tmux ls
 # tmux new -s $session_name
@@ -7,16 +12,18 @@
 # tmux a -t champlain-1   (attach to named session)
 # tmux ctr-b d            (detach)
 
+# Start tmux session
 cd ~/documents/projects/patter-champlain
 tmux new -s champlain-1
 
+# Define arguments
 analysis="sim"
 mobility="116"
 dev="FALSE"
 DIRECTORY_LOG="data/output/$analysis/main/logs/R-CMD-BATCH"
-
 mkdir -p "$DIRECTORY_LOG"
 
+# Run R code
 R CMD BATCH \
   --no-save \
   --no-restore \
