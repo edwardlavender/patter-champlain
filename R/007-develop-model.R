af213412eba12be2e4fab96d2871eb015c5b0952
edwardlavender <edward.lavender@eawag.ch>
2025-05-04 14:51:36 +0200

    As above

--- R/007-develop-model.R
+++ R/007-develop-model.R
@@ -40,45 +40,75 @@ po <- pars_model_obs
 
 pars <- rbind(
   
-  data.table(sensitivity = "best", 
-             mobility = pm$mobility[1], 
-             shape = pm$shape[1], 
-             scale = pm$scale[1], 
-             phi = pm$phi[1], 
-             pars_model_obs),
+  data.table(sensitivity    = "best", 
+             mobility       = pm$mobility[1], 
+             shape          = pm$shape[1], 
+             scale          = pm$scale[1], 
+             phi            = pm$phi[1], 
+             receiver_alpha = po$receiver_alpha[1], 
+             receiver_beta  = po$receiver_beta[1], 
+             receiver_gamma = po$receiver_gamma[1]),
+
+  data.table(sensitivity    = "step(-)", 
+             mobility       = pm$mobility[2], 
+             shape          = pm$shape[2], 
+             scale          = pm$scale[2], 
+             phi            = pm$phi[1], 
+             receiver_alpha = po$receiver_alpha[1], 
+             receiver_beta  = po$receiver_beta[1], 
+             receiver_gamma = po$receiver_gamma[1]),
   
-  data.table(sensitivity = "step(-)", 
-             mobility = pm$mobility[2], 
-             shape = pm$shape[2], 
-             scale = pm$scale[2], 
-             phi = pm$phi[1], 
-             pars_model_obs),
+  data.table(sensitivity    = "step(+)", 
+             mobility       = pm$mobility[3], 
+             shape          = pm$shape[3], 
+             scale          = pm$scale[3], 
+             phi            = pm$phi[1], 
+             receiver_alpha = po$receiver_alpha[1], 
+             receiver_beta  = po$receiver_beta[1], 
+             receiver_gamma = po$receiver_gamma[1]),
   
-  data.table(sensitivity = "step(+)", 
-             mobility = pm$mobility[3], 
-             shape = pm$shape[3], 
-             scale = pm$scale[3], 
-             phi = pm$phi[1], 
-             pars_model_obs),
+  data.table(sensitivity    = "angle(-)", 
+             mobility       = pm$mobility[1], 
+             shape          = pm$shape[1], 
+             scale          = pm$scale[1], 
+             phi            = pm$phi[2], 
+             receiver_alpha = po$receiver_alpha[1], 
+             receiver_beta  = po$receiver_beta[1], 
+             receiver_gamma = po$receiver_gamma[1]),
   
-  data.table(sensitivity = "angle(-)", 
-             mobility = pm$mobility[1], 
-             shape = pm$shape[1], 
-             scale = pm$scale[1], 
-             phi = pm$phi[2], 
-             pars_model_obs),
+  data.table(sensitivity    = "angle(+)", 
+             mobility       = pm$mobility[1], 
+             shape          = pm$shape[1], 
+             scale          = pm$scale[1], 
+             phi            = pm$phi[3], 
+             receiver_alpha = po$receiver_alpha[1], 
+             receiver_beta  = po$receiver_beta[1], 
+             receiver_gamma = po$receiver_gamma[1]),
   
-  data.table(sensitivity = "angle(+)", 
-             mobility = pm$mobility[1], 
-             shape = pm$shape[1], 
-             scale = pm$scale[1], 
-             phi = pm$phi[3], 
-             pars_model_obs)
+  data.table(sensitivity    = "ac(-)", 
+             mobility       = pm$mobility[1], 
+             shape          = pm$shape[1], 
+             scale          = pm$scale[1], 
+             phi            = pm$phi[1], 
+             receiver_alpha = po$receiver_alpha[2], 
+             receiver_beta  = po$receiver_beta[2], 
+             receiver_gamma = po$receiver_gamma[2]),
+  
+  data.table(sensitivity    = "ac(+)", 
+             mobility       = pm$mobility[1], 
+             shape          = pm$shape[1], 
+             scale          = pm$scale[1], 
+             phi            = pm$phi[1], 
+             receiver_alpha = po$receiver_alpha[3], 
+             receiver_beta  = po$receiver_beta[3], 
+             receiver_gamma = po$receiver_gamma[3])
+  
 ) |> 
   mutate(parameter_id = row_number(), .before = "sensitivity") |> 
   as.data.table()
 
 qs::qsave(pars, here_input("pars-patter.qs"))
+
 
 #### End of code.
 ###########################
