86e062f6c54178d7721cdfedd6715f42032c127b
Edward Lavender <e.lavender@napier.ac.uk>
2025-12-18 21:49:18 +0000

    Incorporate detection filters (e.g., false detections)

--- R/002-setup-data-detection.R
+++ R/002-setup-data-detection.R
@@ -32,12 +32,37 @@ library(tictoc)
 files_source_r(here_src())
 
 #### Load data 
-epsg_utm   <- qs::qread(here_input("epsg-utm.qs"))
-map        <- terra::rast(here_input("map.tif"))
-map_bbox   <- qs::qread(here_input("map-bbox.qs"))
-moorings   <- readRDS(here_data_raw_mf("OriginalReceiverSummary_2013-2017.rds"))
-detections <- readRDS(here_data_raw_mf("lkt_detections_2013-2017.rds"))
-surgery    <- fread(here_data_raw("mfutia", "model_comparison", "surgery_log.csv"))
+# lkt_detections_2013-2017.rds: raw detections (93 fish)
+# lkt_detections_2013-2017_filtered.qs: filtered detections (as in Futia et al., 2024)
+epsg_utm            <- qs::qread(here_input("epsg-utm.qs"))
+map                 <- terra::rast(here_input("map.tif"))
+map_bbox            <- qs::qread(here_input("map-bbox.qs"))
+detections          <- readRDS(here_data_raw_mf("lkt_detections_2013-2017.rds"))
+detections_filtered <- qs::qread(here_data_raw_mf("lkt_detections_2013-2017_filtered.qs"))
+moorings            <- readRDS(here_data_raw_mf("OriginalReceiverSummary_2013-2017.rds"))
+surgery             <- fread(here_data_raw("mfutia", "model_comparison", "surgery_log.csv"))
+
+
+###########################
+###########################
+#### Data summary
+
+#### Summary
+# 93 fish were captured, tagged and detected
+# These fish are defined in surgery and detections
+# In the filtered detection dataset, there are 79 individuals
+# For now, the tagged/detected fish data summaries are based on the raw data
+# (optional) TO DO Reorganise this code
+# * Process detections first
+# * Compute summary statistics only for tagged fish ultimately included in the analysis
+# * See also prepare-analysis.R
+# This is not currently implemented b/c in the manuscript we provide the full
+# dataset size & then explain the filtering steps. 
+
+# In the filtered detection dataset, there are 79 individuals
+# For now, the tagged fish summary includes 93 fish 
+# (optional) TO DO Move this code around so detections is processed first
+# and the supporting tables are only produced for analysed fish 
 
 
 ###########################
@@ -360,8 +385,9 @@ stopifnot(nr > 1L)
 
 ###########################
 ###########################
-#### Raw data summary statistics
+#### Apply filters 
 
+#### Raw data summary statistics
 # 1,735,137 detections
 # from 93 individuals
 # derived from 153 receiver deployments 
@@ -372,6 +398,27 @@ length(unique(detections$individual_id))
 range(detections$timestamp)
 difftime(max(detections$timestamp), min(detections$timestamp), units = "days")
 nrow(moorings_real)
+
+#### False detections 
+# Check study duration
+c(study_start, study_end)
+range(detections_filtered$detection_timestamp_utc)
+# Format detections_filtered
+detections_filtered |> setDT()
+detections_filtered[, individual_id := as.integer(as.character(animal_id))]
+# Filter detections using detections_filtered (inner join)
+# * This ensures we carry forward false detection filters etc. implemented by Futia et al. (2024)
+detections <- detections[
+  detections_filtered,
+  on = .(individual_id = individual_id, timestamp = detection_timestamp_utc),
+  nomatch = 0
+]
+
+#### Summarise filtered detection dataset
+nrow(detections_filtered)
+nrow(detections)
+length(unique(detections$individual_id))
+length(unique(detections_filtered$individual_id))
 
 
 ###########################
