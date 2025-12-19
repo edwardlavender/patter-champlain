d622ca07ace59781b84aaba9513fa63745119342
Edward Lavender <e.lavender@napier.ac.uk>
2025-12-19 12:05:45 +0000

    Add comment

--- src/stats.R
+++ src/stats.R
@@ -12,6 +12,7 @@ dmix <- function(x, mean = 0, sd = 0.3) {
   # dnorm(x, mean = mean, sd = sd)
   d1 <- truncdist::dtrunc(x, spec = "norm", a = -pi, b =  pi, mean = mean, sd = sd)
   d2 <- dunif(x, -pi, pi)
+  # w1 = 0.25 empiricaly matches the VPS data
   w1 <- 0.99
   w2 <- 1 - w1
   d1 * w1 + d2 * w2
