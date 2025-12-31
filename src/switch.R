0fc1a081930289ec25ddd6f5898bf6ec0b2c98e7
Edward Lavender <e.lavender@napier.ac.uk>
2025-12-31 10:38:19 +0100

    Revise switch function

--- src/switch.R
+++ src/switch.R
@@ -1,21 +1,7 @@
-switch_function <- function(analysis = c("sim", "real"), fun_sim, fun_real) {
-  analysis <- match.arg(analysis)
-  switch(
-    analysis,
-    sim  = fun_sim,
-    real = fun_real,
-    stop("`analysis` must be 'sim' or 'real'")
-  )
-}
-
-# switch_constructor_ac_analysis <- function(analysis) {
-#   switch_function(analysis, constructor_ac_sim, constructor_ac_real)
-# }
-
-switch_here_input_analysis <- function(analysis) {
-  switch_function(analysis, here_input_sim, here_input_real)
-}
-
-switch_here_output_analysis_main <- function(analysis) {
-  switch_function(analysis, here_output_sim_main, here_output_real_main)
+switch_here_input_analysis_subanalysis <- function(analysis = c("sim", "real"), subanalysis = "main") {
+  analysis    <- match.arg(analysis)
+  subanalysis <- match.arg(subanalysis)
+  switch(analysis, 
+         sim = function(...) here_input(analysis, subanalysis, ...), 
+         real = function(...) here_input(analysis, subanalysis, ...))
 }
