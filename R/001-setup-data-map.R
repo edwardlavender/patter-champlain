2c90bb5929c6974987e161a817268799851c9b34
Edward Lavender <e.lavender@napier.ac.uk>
2025-12-31 14:20:30 +0000

    Add comment

--- R/001-setup-data-map.R
+++ R/001-setup-data-map.R
@@ -36,6 +36,7 @@ champlain  <- terra::vect(here_data_raw_mf("ChamplainRegionsGrouped/ChamplainReg
 #### Define study area (~2 s)
 
 #### Define UTM SpatVector
+# NB: as.numeric(1) is needed for Patter.particle_filter()
 epsg_utm         <- "EPSG:3175"
 champlain$land   <- as.numeric(1)
 champlain_utm    <- champlain |> terra::project(epsg_utm)
