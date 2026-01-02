771d826b82858edfd7550b24f2997c76f6782950
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-02 11:06:08 +0000

    Add monitor-patter.R

---
+++ R/019-monitor-patter.R
@@ -0,0 +1,62 @@
+###########################
+###########################
+#### monitor-patter.R
+
+#### Aims
+# 1) This script monitors particle algorithm progress
+
+#### Prerequisites
+# 1) Run previous scripts
+
+
+###########################
+###########################
+#### Set up 
+
+#### Wipe workspace 
+rm(list = ls())
+
+#### Set global options
+Sys.setenv("JULIA_SESSION" = FALSE)
+set.seed(123L)
+
+#### Load essential packages
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
+library(proj.verse)
+library(tictoc)
+files_source_r(here_src())
+
+#### Load data
+map <- terra::rast(here_input("map.tif"))
+
+
+###########################
+###########################
+#### Select analysis
+
+#### Define analysis 
+analysis <- "sim"
+# analysis <- "real"
+subanalysis <- "main"
+
+#### Define analysis-specific data
+here_input_analysis <- switch_here_input_analysis_subanalysis(analysis, subanalysis)
+iteration           <- qs::qread(here_input_analysis("iteration.qs"))
+
+
+###########################
+###########################
+#### Monitor progress
+
+#### Monitor progress
+table(file.exists(iteration$file_callstats))
+table(file.exists(iteration$file_diagnostics))
+table(file.exists(iteration$file_states))
+table(file.exists(iteration$file_occupancy))
+
+
+#### End of code. 
+###########################
+###########################
