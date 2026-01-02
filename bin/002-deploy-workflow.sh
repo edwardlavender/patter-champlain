4e707b242fc29b69869856c2579ffe64fcb8e70e
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-02 15:36:19 +0000

    Add reminders

--- bin/002-deploy-workflow.sh
+++ bin/002-deploy-workflow.sh
@@ -1,5 +1,23 @@
-cd ~/documents/projects/patter-champlain
-tmux ls
-tmux new -s ch
-chmod +x ./bin/002-deploy-workflow.sh
-./bin/002-deploy-workflow.sh
+# Set analysis = "sim" or analysis = "real" in scripts: 
+# * 015-prepare-analysis.R
+# * 001-run-algorithms.jl
+# * 002-collate-states.jl
+# * 003-run-patter.R 
+
+# Unlink old data/output/{analysis}/main/ (if needed):
+# file.path("data", "output", "sim", "main", "runs")
+# file.path("data", "output", "real", "main", "runs")
+
+# Rebuild data/output/{analysis}/main/ directories:
+# * Run 015-prepare-analysis.R
+
+# Set NROW in 002-deploy-workflow.sh
+# NROW=210
+# NROW=2723
+
+# Open tmux and run workflow:
+# cd ~/documents/projects/patter-champlain
+# tmux ls
+# tmux new -s ch
+# chmod +x ./bin/002-deploy-workflow.sh
+# ./bin/002-deploy-workflow.sh
