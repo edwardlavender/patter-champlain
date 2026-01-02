4b28bcc7fe6dce87b3bccd3731bb03f4db73c4f6
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-02 14:17:50 +0000

    Add results from real iteration[1, ]

--- R/017-plan-patter.R
+++ R/017-plan-patter.R
@@ -40,8 +40,8 @@ map <- terra::rast(here_input("map.tif"))
 #### Select analysis
 
 #### Define analysis 
-analysis <- "sim"
-# analysis <- "real"
+# analysis <- "sim"
+analysis <- "real"
 subanalysis <- "main"
 
 #### Define analysis-specific data
@@ -70,68 +70,84 @@ if (FALSE) {
 #### Collate states 
 # Run 001-run-algorithms.jl for example individual & check file size for it$file_states (MB)
 if (FALSE) {
-  # For sim, iteration[1, ]: 678.995 MB
   file.size(it$file_states) / 1e6 
   
-  # There is little benefit in rewriting as qs: 
+  # For sim iteration[1, ], there is little benefit in rewriting as .qs:
+  it_states <- arrow::read_feather(it$file_states)
   tmp.qs <- tempfile(fileext = ".qs")
-  qs::qsave(it_states, tmp.qs, preset = "custom", algorithm = "zstd", compress_level = 9L) # 628.6245 MB
+  qs::qsave(it_states, tmp.qs, preset = "custom", algorithm = "zstd", compress_level = 9L) #  628.6245 MB
   qs::qsave(it_states, tmp.qs, preset = "archive") # 625.6087
-  qs::qread(tmp.qs)
   file.size(tmp.qs) / 1e6
+  #  qs::qread(tmp.qs)
 }
 
 
 ###########################
 ###########################
-#### Examine outputs
+#### Read outputs 
 
-#### Read data 
+#### Read outputs 
 it <- iteration[1, ]
 it_callstats   <- arrow::read_feather(it$file_callstats)
 it_diagnostics <- arrow::read_feather(it$file_diagnostics)
 it_states      <- arrow::read_feather(it$file_states)
-# Check file sizes
-# > We should be able to read all diagnostic files into memory 
-nrow(it_diagnostics) # 66960
-lobstr::obj_size(it_diagnostics) 
-nrow(it_states)
 
-#### Examine computation time (SIA-LAVENDED, 50000/1500 particles, 1 thread, 10 batches)
-## Check outputs
+# Check object sizes
+# > We should be able to read all diagnostic files into memory 
+nrow(it_diagnostics)             # 66960 rows
+lobstr::obj_size(it_diagnostics) # 1.34-3.5 MB
+nrow(it_states)                  # 44640000 rows
+
+
+###########################
+###########################
+#### Examine computation time
+
+#### Check callstats
+# sim: SIA-LAVENDED, 50000/1500 particles, 1 thread, 10 batches
+# real: SIA-LAVENDED, 75000/2000 particles, 1 thread, 10 batches
 it_callstats
 sum(it_callstats$time)
-#
-## Simulations: 
-#
-# For iteration[1, ]:
-#
+
+#### Simulations, iteration[1, ]:
+
 # timestamp           routine              n_particle n_iter   loglik convergence  time
 # <dttm>              <chr>                     <int>  <dbl>    <dbl> <lgl>       <dbl>
 # 1 2026-01-02 10:46:43 filter: forward           50000      1 -108537. TRUE         910.
 # 2 2026-01-02 11:01:53 filter: backward          50000      1 -106402. TRUE         642.
 # 3 2026-01-02 11:12:36 smoother: two-filter       1500    NaN     NaN  TRUE        3433.
-#
+
 # > Total : 4984.926 s (1.38 hours) for iteration[1, ]
 # > ETA   : 0.60 days for 210 iterations on 20 cores
 (4984.926 * 210)  / 60 / 60 / 24 / 20
-#
-## Real-world analysis
-# * TO DO for it[1, ] 
-# * >>8 days (ETA) for 2723 iterations on 20 cores
-(4984.926 * 2723)  / 60 / 60 / 24 / 20
 
+#### Real-world analysis, iteration[1, ]
+
+# timestamp           routine              n_particle n_iter   loglik convergence  time
+# <dttm>              <chr>                     <int>  <dbl>    <dbl> <lgl>       <dbl>
+# 1 2026-01-02 12:33:19 filter: forward           75000      1 -195703. TRUE        1069.
+# 2 2026-01-02 12:51:08 filter: backward          75000      1 -195507. TRUE        1079.
+# 3 2026-01-02 13:09:07 smoother: two-filter       2000    NaN     NaN  TRUE        6631.
+
+# > Total : 8779.24 s (2.438678 hours) for iteration[1, ]
+# > ETA   : 13.83442 days for 2723 iterations on 20 cores
+(8779.24 * 2723)  / 60 / 60 / 24 / 20
+
+
+###########################
+###########################
 #### Examine convergence 
-#
-## For sim iteration[1, ]:
+
+#### For sim iteration[1, ]:
 # percent_valid convergence
 # <num>      <lgcl>
-#   1:      99.98656        TRUE
-#
-# For real iteration[1, ]:
-#
-# TO DO
-# 
+#  99.98656        TRUE
+
+#### For real iteration[1, ]:
+# percent_valid convergence
+# <num>      <lgcl>
+#  100        TRUE
+
 it_callstats$convergence[1]
 it_callstats$convergence[2]
 it_diagnostics |>
@@ -140,32 +156,42 @@ it_diagnostics |>
             convergence = percent_valid > 75) |> 
   as.data.table()
 
+
+###########################
+###########################
 #### Examine ESS
-#
-## For sim it[1, ]:
-#
+
+#### For sim iteration[1, ]:
 # routine                min   mean median    max     sd    IQR    MAD
 # <chr>                <dbl>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>
-#   1 filter: backward      152. 14137.  8793. 49933. 13181. 17798. 9236. 
+# 1 filter: backward      152. 14137.  8793. 49933. 13181. 17798. 9236. 
 # 2 filter: forward       277. 14095.  8833. 49935. 13123. 17606. 9285. 
 # 3 smoother: two-filter    1    214.   182.  1500    159.   111.   82.8
-#
-## For real it[1, ]
-# 
-# TO DO
-#
+
+#### For real iteration[1, ]:
+# routine                 min   mean median    max     sd    IQR    MAD
+# <chr>                 <dbl>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>
+# 1 filter: backward     380.   20271. 11796. 74866. 19741. 26389. 12700.
+# 2 filter: forward      401.   20498. 12186. 74769. 19864. 26697. 13239.
+# 3 smoother: two-filter   1.63   694.   703.  2000    376.   643.   469.
+
 it_diagnostics |> 
   group_by(routine) |> 
   reframe(utils.add::basic_stats(ess, na.rm = TRUE))
+
+
+###########################
+###########################
+#### Mapping 
 
 #### Map states (~7 s)
 # Each map requires 0.069128 MB of storage
 tic()
 tmp.tif <- tempfile(fileext = ".tif")
-tmp     <- map_pou(map, it_states)$ud
+tmp     <- patter::map_pou(map, it_states)$ud
 terra::writeRaster(tmp, tmp.tif, overwrite = TRUE)
 file.size(tmp.tif) / 1e6
-# map_dens(map, it_states, .discretise = TRUE)
+# patter::map_dens(map, it_states, .discretise = TRUE)
 toc()
 
 #### Map states + trajectory
@@ -188,14 +214,14 @@ if (analysis == "sim") {
 #### Define the number of particles for algorithms
 # We run algorithms on {n_cpu}
 # We record {n_record} particles
-# We quadrouple the required memory
+# We quadruple the required memory
 # - We may start a new run while still finishing another (x2)
 # - We need a safety buffer (x3)
 # We use a maximum of {mb_available} memory in total
-n_record     <- 1500
+n_record     <- 2000
 n_cpu        <- 20
 mb_available <- 25e3
-mb_required  <- p_mem(n_record, 22320, 4, n_cpu) * 4
+mb_required  <- p_mem(n_record, 22320, 4, n_cpu) * 3
 p_batch(mb_required, mb_available)
 
 #### Storage requirements for simulations (1500 particles):
@@ -211,9 +237,20 @@ p_batch(mb_required, mb_available)
 (0.00209 + 0.896738 + 678.995 + 0.068324) * 210 / 1e3 
 
 #### Storage requirements for real-world analysis (2000 particles): 
-# To store the outputs of the real world analysis, we would need 2468.654 GB
+# Breakdown (Arrow.write compress = ZstdCompressor(level = 9))
+# * callstats   : 0.00209  
+# * diagnostics : 1.071778
+# * jld2 files  : 1428.534
+# * states      : 927.1699
+# * map         : 0.036435
+# * Total       : 928.2802
+#   (excluding jld2 files, which we delete)
+# To store the outputs of the real world analysis, we would need 2527.707 GB
+# A) Guess based on simulation analysis with 1500 versus 2000 particles:
 z <- 2000/1500
 (0.00209 * z + 0.896738 * z + 678.995 * z + 0.068324) * 2723 / 1e3 
+# B) Estimate based on real iteration[1, ]
+(0.00209 + 1.071778 + 927.1699 + 0.036435) * 2723 / 1e3 
 
 #### Define implementation strategy
 # Handle iterations in batches e.g., 1:100, 101:200, ..., N
@@ -226,7 +263,7 @@ z <- 2000/1500
 # * Each batch requires only ~200 GB of storage space: 1878.502 * 100 / 1e3 
 # * All batches ultimately require only:
 # - (0.00189 + 3.550282 + 0.068324) * 210 = 760 MB (simulations)
-# - (0.00189 + 3.550282 + 0.068324) * 2723 = 9858.611 MB (real-world analysis)
+# - (0.00209 + 1.071778 + 0.036435) * 2723 = 3023.355 MB (real-world analysis)
 
 
 #### End of code. 
