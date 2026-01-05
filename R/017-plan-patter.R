82cab6179122501ca6e06af39bfb60e17f2a000b
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-05 15:21:29 +0000

    Update script in line with revised Julia workflows

--- R/017-plan-patter.R
+++ R/017-plan-patter.R
@@ -10,6 +10,7 @@
 
 #### Prerequisites
 # 1) Run simulations
+# 2) This code is designed for SIA-LAVENDED 
 
 
 ###########################
@@ -20,13 +21,15 @@
 rm(list = ls())
 
 #### Set global options
-Sys.setenv("JULIA_SESSION" = FALSE)
+Sys.setenv(JULIA_SESSION = "FALSE")
 set.seed(123L)
 
 #### Load essential packages
 library(data.table)
 library(dtplyr)
 library(dplyr, warn.conflicts = FALSE)
+library(glue)
+library(JuliaCall)
 library(proj.verse)
 library(tictoc)
 files_source_r(here_src())
@@ -40,8 +43,8 @@ map <- terra::rast(here_input("map.tif"))
 #### Select analysis
 
 #### Define analysis 
-# analysis <- "sim"
-analysis <- "real"
+analysis <- "sim"
+# analysis <- "real"
 subanalysis <- "main"
 
 #### Define analysis-specific data
@@ -54,7 +57,8 @@ iteration           <- qs::qread(here_input_analysis("iteration.qs"))
 #### Generate example outputs
 
 #### Define example individual
-it <- iteration[1, ]
+i  <- 1
+it <- iteration[i, ]
 list.files(it$folder_output)
 
 #### Run algorithms 
@@ -65,12 +69,29 @@ if (FALSE) {
   file.size(it$file_callstats) / 1e6 
   file.size(it$file_diagnostics) / 1e6
   sum(file.size(list.files(it$folder_output, pattern = "\\.jld2$", full.names = TRUE))) / 1e6
+  sum(file.size(list.files(it$folder_output, pattern = "pou", full.names = TRUE)) / 1e6)
 }
 
 #### Collate states 
 # Run 001-run-algorithms.jl for example individual & check file size for it$file_states (MB)
 if (FALSE) {
+  
+  # Connect to Julia 
+  # * Connecting to Julia can cause issues with geospatial operations below (even on SIA-LAVENDED)
+  # * If needed:
+  #   - Implement this code
+  #   - Restart R
+  #   - Continue analysis below using produced file (it$file_states)
+  patter::julia_connect()
+  
+  # Make it$file_states & compute MB (~11 s) 
+  tic()
+  julia_source("./Julia/src/utils.jl")
+  julia_assign("iteration", iteration)
+  julia_command(glue('iter = iteration[{i}, :]'))
+  julia_command('write_file_states(iter)')
   file.size(it$file_states) / 1e6 
+  toc()
   
   # For sim iteration[1, ], there is little benefit in rewriting as .qs:
   it_states <- arrow::read_feather(it$file_states)
@@ -88,15 +109,19 @@ if (FALSE) {
 
 #### Read outputs 
 it <- iteration[1, ]
-it_callstats   <- arrow::read_feather(it$file_callstats)
-it_diagnostics <- arrow::read_feather(it$file_diagnostics)
-it_states      <- arrow::read_feather(it$file_states)
+it_timeline    <- arrow::read_feather(it$file_timeline)$timestamp
+it_callstats   <- arrow::read_feather(it$file_callstats) |> setDT()
+it_diagnostics <- arrow::read_feather(it$file_diagnostics) |> setDT()
+it_states      <- NULL
+if (file.exists(it$file_states)) {
+  it_states    <- arrow::read_feather(it$file_states) |> setDT()
+}
 
 # Check object sizes
 # > We should be able to read all diagnostic files into memory 
 nrow(it_diagnostics)             # 66960 rows
 lobstr::obj_size(it_diagnostics) # 1.34-3.5 MB
-nrow(it_states)                  # 44640000 rows
+# nrow(it_states)                # 44640000 rows
 
 
 ###########################
@@ -184,15 +209,52 @@ it_diagnostics |>
 ###########################
 #### Mapping 
 
-#### Map states (~7 s)
-# Each map requires 0.069128 MB of storage
-tic()
-tmp.tif <- tempfile(fileext = ".tif")
-tmp     <- patter::map_pou(map, it_states)$ud
-terra::writeRaster(tmp, tmp.tif, overwrite = TRUE)
-file.size(tmp.tif) / 1e6
-# patter::map_dens(map, it_states, .discretise = TRUE)
-toc()
+#### Map states 
+if (Sys.getenv("JULIA_SESSION") == "FALSE") {
+  
+  # Compute a data.table of POU (x, y, mark = probability mass)
+  coord <- 
+    list.files(it$folder_output, full.names = TRUE, pattern = "pou-") |> 
+    lapply(arrow::read_feather) |>
+    rbindlist() |> 
+    arrange(timestep, x, y) |> 
+    group_by(x, y) |> 
+    summarise(mark = sum(mark)) |> 
+    ungroup() |> 
+    mutate(mark = mark / length(it_timeline)) |> 
+    as.data.table()
+    
+  # Verify that weights sum to one 
+  stopifnot(all.equal(1, sum(coord$mark)))
+
+  # Map occupancy 
+  occupancy <- terra::rasterize(coord, map, values = coord$mark)
+  occupancy <- terra::classify(occupancy, cbind(NA, 0))
+  occupancy <- terra::mask(occupancy, map)
+  names(occupancy) <- "map_value"
+  terra::plot(occupancy)
+  
+  # Each map requires 0.069128 MB of storage
+  occupancy.tif <- tempfile(fileext = ".tif")
+  terra::writeRaster(occupancy, occupancy.tif, overwrite = TRUE)
+  file.size(occupancy.tif) / 1e6
+  # patter::map_dens(map, it_states, .discretise = TRUE)
+  
+  # Validate occupancy map with via patter::map_pou()
+  # > The map produced from the summarised Julia outputs
+  #   should match that produced using all states from patter
+  if (!is.null(it_states)) {
+    # Use map_pou() to verify occupancy 
+    occupancy_2 <- patter::map_pou(map, it_states, .plot = FALSE)$ud
+    stopifnot(terra::all.equal(occupancy, occupancy_2, maxcell = terra::ncell(occupancy)))
+    # Visual check
+    pp <- par(mfrow = c(1, 2))
+    terra::plot(occupancy)
+    terra::plot(occupancy_2)
+    par(pp)
+  }
+  
+}
 
 #### Map states + trajectory
 # This is a quick validation analysis
@@ -202,7 +264,7 @@ if (analysis == "sim") {
   paths <- qs::qread(here_input_analysis("paths.qs"))
   path  <- paths[path_id == it$individual_id[1], ]
   # Plot map & paths
-  terra::plot(tmp)
+  terra::plot(occupancy)
   patter:::add_sp_path(path$x, path$y, length = 0.01, lwd = 0.1)
 }
 
@@ -227,25 +289,27 @@ p_batch(mb_required, mb_available)
 #### Storage requirements for simulations (1500 particles):
 # Breakdown (Arrow.write default, Arrow.write compress = ZstdCompressor(level = 9))
 # * callstats   : 0.00189,  0.00209
-# * diagnostics : 3.550282, 0.896738
+# * diagnostics : unknown,  0.955378 (formerly 0.896738 without two area metrics)
 # * jld2 files  : 1071.414, 1071.414 (same)
 # * states      : 1874.882, 678.995
+# * pou files   : unknown,  24.10011
 # * map         : 0.068324, 0.068324 (see below)
 # * Total       : 1878.502, 679.9622
 #   (excluding jld2 files, which we delete)
-# To store the outputs of the simulations, we need ~142.7921 GB
-(0.00209 + 0.896738 + 678.995 + 0.068324) * 210 / 1e3 
+# To store all outputs of the simulations, we would need ~142.7921 GB
+(0.00209 + 0.955378 + 678.995 + 0.068324) * 210 / 1e3 
 
 #### Storage requirements for real-world analysis (2000 particles): 
 # Breakdown (Arrow.write compress = ZstdCompressor(level = 9))
 # * callstats   : 0.00209  
-# * diagnostics : 1.071778
+# * diagnostics : unknown (formerly 1.071778)
 # * jld2 files  : 1428.534
 # * states      : 927.1699
+# * pou files   : TO DO (unknown)
 # * map         : 0.036435
 # * Total       : 928.2802
 #   (excluding jld2 files, which we delete)
-# To store the outputs of the real world analysis, we would need 2527.707 GB
+# To store all outputs of the real world analysis, we would need ~2527.707 GB
 # A) Guess based on simulation analysis with 1500 versus 2000 particles:
 z <- 2000/1500
 (0.00209 * z + 0.896738 * z + 678.995 * z + 0.068324) * 2723 / 1e3 
@@ -253,17 +317,43 @@ z <- 2000/1500
 (0.00209 + 1.071778 + 927.1699 + 0.036435) * 2723 / 1e3 
 
 #### Define implementation strategy
-# Handle iterations in batches e.g., 1:100, 101:200, ..., N
-# For each batch:
-# * Run particle algorithms in parallel (Julia)
-# * Collate states (Julia)
-# * Make maps & compute diagnostics (R)
+# Run iterations in parallel 
+# For each iteration
+# * Run particle algorithms using batching & record:
+#   - callstats
+#   - diagnostics, including area spanned by smoothed distributions
+#   - pou data.tables by batch, which we can use to collate maps in R
 # * Unlink large particle files (R) 
-# This minimises disk space requirements
-# * Each batch requires only ~200 GB of storage space: 1878.502 * 100 / 1e3 
-# * All batches ultimately require only:
-# - (0.00189 + 3.550282 + 0.068324) * 210 = 760 MB (simulations)
-# - (0.00209 + 1.071778 + 0.036435) * 2723 = 3023.355 MB (real-world analysis)
+# This minimises memory & disk space requirements for effective parallelisation
+# - Particle outputs are only produced temporarily
+# - We analyse outputs immediately without reading all particle into memory
+# - We delete particle files on the fly after analysis 
+# - Later in R, we analyse diagnostics & collate maps using smaller outputs
+
+#### Summarise storage requirements (GB) for implementation strategy
+
+## (1) Temporary storage requirements
+# We need temporary storage for all CPUs producing:
+# - callstats.feather
+# - diagnostics.feather
+# - fwd-{i}.jld2, bwd-{i}.jld2, smo-{i}.jld2 matrices
+# - pou-{i}.feather files
+(0.00209 + 0.955378 + 1071.414 * 3 + 24.10011) * n_cpu / 1e3 # sim, 64.78599 GB
+(0.00209 + 0.955378 + 1428.534 * 3 + 24.10011) * n_cpu / 1e3 # real, 86.21319 GB
+
+## (2) Ultimate Julia iteration requirements (GB)
+# All Julia iterations ultimately require storage for:
+# - callstats.feather
+# - diagnostics.feather
+# - pou-{i}.feather files
+(0.00209 + 0.955378 + 24.10011) * 210 / 1e3  # sim, 5.262091 GB
+(0.00209 + 0.955378 + 24.10011) * 2723 / 1e3 # real, 68.23178 GB
+
+## (3) Ultimate iteration requirements after R mapping 
+# These are as in (2) but we include occupancy.tif files
+# (We could clean up pou-{i}.feather files)
+(0.00209 + 0.955378 + 24.10011 + 0.068324) * 210 / 1e3  # sim, 5.276439 GB
+(0.00209 + 0.955378 + 24.10011 + 0.036435) * 2723 / 1e3 # real, 68.331 GB
 
 
 #### End of code. 
