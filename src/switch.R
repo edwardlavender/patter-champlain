55b4a00f545fb2730f473dcc522e8d3d214db7cb
edwardlavender <edlavender01@gmail.com>
2026-01-06 19:38:37 +0100

    Add function

--- src/switch.R
+++ src/switch.R
@@ -5,3 +5,11 @@ switch_here_input_analysis_subanalysis <- function(analysis = c("sim", "real"), 
          sim = function(...) here_input(analysis, subanalysis, ...), 
          real = function(...) here_input(analysis, subanalysis, ...))
 }
+
+switch_here_fig_analysis_subanalysis <- function(analysis = c("sim", "real"), subanalysis = "main") {
+  analysis    <- match.arg(analysis)
+  subanalysis <- match.arg(subanalysis)
+  switch(analysis, 
+         sim = function(...) here_fig(analysis, subanalysis, ...), 
+         real = function(...) here_fig(analysis, subanalysis, ...))
+}
