c4e0ed73767166d2109c9338b2ac60fe87bdb845
edwardlavender <edlavender01@gmail.com>
2026-01-07 14:27:52 +0100

    Check log files

--- R/019-monitor-patter.R
+++ R/019-monitor-patter.R
@@ -42,8 +42,9 @@ analysis <- "sim"
 subanalysis <- "main"
 
 #### Define analysis-specific data
-here_input_analysis <- switch_here_input_analysis_subanalysis(analysis, subanalysis)
-iteration           <- qs::qread(here_input_analysis("iteration.qs"))
+here_input_analysis  <- switch_here_input_analysis_subanalysis(analysis, subanalysis)
+here_output_analysis <- switch_here_output_analysis_subanalysis(analysis, subanalysis)
+iteration            <- qs::qread(here_input_analysis("iteration.qs"))
 
 
 ###########################
@@ -53,6 +54,30 @@ iteration           <- qs::qread(here_input_analysis("iteration.qs"))
 #### Monitor progress
 table(file.exists(iteration$file_callstats))
 table(file.exists(iteration$file_diagnostics))
+
+#### Check rows with missing files
+iteration[!file.exists(file_callstats), .(index, unit_id, individual_id, time_id, sensitivity)]
+
+#### Check for errors
+# Read logfiles 
+logfiles <- list.files(here_output_analysis("logs"), full.names = TRUE, pattern = "\\.log$")
+logfiles <- gtools::mixedsort(logfiles, decreasing = TRUE)
+rows <- 
+  logfiles |>
+  basename() |>
+  stringr::str_extract("\\d+(?=\\.log$)") |> 
+  as.integer()
+logs <- lapply(logfiles, readLines)
+names(logs) <- as.character(rows)
+# Check for missing logs
+table(iteration$index %in% rows)
+# Read logs
+logs
+# Search for 'error', 'fail' or simular
+logtxt <- do.call(paste, lapply(logs, function(l) paste0(l, collapse = ", ")))
+stringr::str_detect(tolower(logtxt), "error")
+stringr::str_detect(tolower(logtxt), "fail")
+stringr::str_detect(tolower(logtxt), "failure")
 
 
 #### End of code. 
