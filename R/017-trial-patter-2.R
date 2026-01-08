a0307d46931fa56ae9246fe0398619c63d276e6a
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-08 15:03:50 +0000

    Add trial-patter-2.R script & renumber scripts

---
+++ R/017-trial-patter-2.R
@@ -0,0 +1,140 @@
+###########################
+###########################
+#### trial-patter.R
+
+#### Aims
+# 1) This script is used to trial/debug the particle filter i.e., convergence failures
+
+#### Prerequisites
+# 1) Set JULIA_NUM_THREADS = 12
+
+
+###########################
+###########################
+#### Set up 
+
+#### Wipe workspace 
+rm(list = ls())
+
+#### Set global options
+Sys.setenv("JULIA_SESSION" = FALSE)
+set.seed(123L)
+
+#### Load essential packages
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
+library(patter)
+library(proj.verse)
+library(tictoc)
+files_source_r(here_src())
+
+#### Load data
+map <- terra::rast(here_input("map.tif"))
+
+
+###########################
+###########################
+#### Select analysis
+
+#### Define analysis 
+analysis <- "sim"
+# analysis <- "real"
+subanalysis <- "main"
+
+#### Define analysis-specific data
+here_input_analysis <- switch_here_input_analysis_subanalysis(analysis, subanalysis)
+here_fig_analysis   <- switch_here_fig_analysis_subanalysis(analysis, subanalysis)
+iteration           <- qs::qread(here_input_analysis("iteration.qs"))
+
+
+###########################
+###########################
+#### Run filter
+
+#### Connect to Julia
+julia_connect()
+set_seed()
+set_map(map)
+
+#### Read data for selected individual
+it <- iteration[individual_id == 26 & sensitivity == "best", ]
+timeline  <- arrow::read_feather(it$file_timeline)
+timeline  <- timeline$timestamp
+acoustics <- arrow::read_feather(it$file_acoustics)
+containers_fwd <- arrow::read_feather(it$file_containers_fwd)
+containers_bwd <- arrow::read_feather(it$file_containers_bwd)
+
+#### Define movement model
+state      <- "StateCXY"
+model_move <- model_move_trout(it)
+model_move
+
+#### Define observation(s) & observation model
+# (Containers are added below)
+yobs <- list(ModelObsAcousticLogisTrunc = copy(acoustics), 
+             ModelObsContainer = NULL)
+
+#### Run filter 
+# Define direction & update yobs 
+direction <- "backward"
+stopifnot(direction %in% c("forward", "backward"))
+if (direction == "forward") {
+  yobs$ModelObsContainer <- copy(containers_fwd)
+} else {
+  yobs$ModelObsContainer <- copy(containers_bwd)
+}
+# Define arguments
+pargs <- list(.timeline   = timeline,
+              .state      = state,
+              .model_move = model_move,
+              .yobs       = yobs,
+              .n_move     = 1L,
+              .n_particle = it$n_particle_filter,
+              .n_record   = it$n_particle_smoother,
+              .direction  = direction)
+# Run filter
+pout <- do.call(pf_filter, pargs, quote = TRUE)
+
+#### Simulation record
+## index 176, individual 26, sensitivity == "best"
+# - default settings, .n_move = 1L, .n_particle = 50000
+# - SIA-LAVENDED: success
+# - siam-linux20: failure (assume unlucky)
+
+#### Real-world record (100 test time series)
+# TO DO 
+
+
+###########################
+###########################
+#### Animation
+
+# Create animation
+# * For 12,000 steps: This takes 6 min (12 cl) plus >2 min for 12,000 steps
+# * NB: Running this for a few steps with .cl = 1L seems to suppress a segmentation
+#   fault when it then run in parallel for a larger time series below.
+#   If you jump to the parallel version, it can throw a segmentation fault
+steps <- 1:1000L
+tnow <- as.numeric(Sys.time())
+animate_ac(.iter   = it,
+           .map    = map,
+           .steps  = steps[1:10L],
+           .input  = pargs,
+           .output = pout,
+           .outdir = here_fig_analysis("debug", it$individual_id, it$time_id, tnow),
+           .cl     = 1L)
+if (length(steps) > 10L) {
+  animate_ac(.iter   = it,
+             .map    = map,
+             .steps  = steps,
+             .input  = pargs,
+             .output = pout,
+             .outdir = here_fig_analysis("debug", it$individual_id, it$time_id, tnow),
+             .cl     = 10L)
+}
+
+
+#### End of code. 
+###########################
+###########################
--- src/animate.R
+++ src/animate.R
@@ -20,29 +20,34 @@ if (!patter:::os_linux() | (patter:::os_linux() & !patter:::julia_session())) {
   
   # Animate particles from the particle filter for the AC algorithm
   # * Code adapted for debugging from https://github.com/edwardlavender/patter-flapper/blob/main/src/debug-convergence.R
-  # @param .sim The data.table row
+  # @param .iter The data.table row
   # @param .map The SpatRaster
-  # @param .moorings The moorings data.table
-  # @param .start,.stop Integers that define the time steps of interest
+  # @param .steps Integers that define the time steps of interest
   # @param .input The named list of arguments passed to pf_filter()
   # @param .output The named list of outputs from pf_filter()
+  # @param .outdir The output directory 
   
-  animate_ac <- function(.sim, 
+  animate_ac <- function(.iter, 
                          .map, 
                          .steps,
                          .input, .output, 
-                         .tnow,
+                         .outdir,
                          .cl = 1L) {
 
     # Checks
     .steps <- .steps[.steps <= length(.input$.timeline)]
     
     # Create directories
-    frames <- here_fig("debug", .tnow, "frames")
+    dir.create(.outdir, recursive = TRUE)
+    frames <- file.path(.outdir, "frames")
     unlink(frames, recursive = TRUE)
     dir.create(frames, recursive = TRUE)
-    mp4 <- here_fig("debug", .tnow)
-    dir.create(mp4)
+    mp4 <- .outdir
+    
+    # Record parameter settings
+    sink(file.path(.outdir, "arguments.txt"))
+    print(.input)
+    sink()
     
     # Define datasets
     timeline   <- .input$.timeline
