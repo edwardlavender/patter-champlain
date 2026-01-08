e0bcd4d63980e4576369214e866d0068506d8bbf
Edward Lavender <e.lavender@napier.ac.uk>
2026-01-08 15:42:38 +0000

    Update script numbering

--- bin/001-workflow.sh
+++ bin/001-workflow.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT="./Julia/002-run-algorithms.jl"
+# SCRIPT="./Julia/001-run-filter.jl" 
 ANALYSIS="sim"
 NROW=4
-LOGDIR="./data/output/$ANALYSIS/main/logs"
 NCPU=2
+LOGDIR="./data/output/$ANALYSIS/main/logs"
 mkdir -p "$LOGDIR"
 
 export JULIA_NUM_THREADS=1
@@ -13,6 +15,6 @@ seq 1 "$NROW" |
   xargs -P "$NCPU" -I {} \
     bash -c '
       LOGFILE="'"$LOGDIR"'/log-$1.log"
-      julia "./Julia/001-run-algorithms.jl" "$1" >"$LOGFILE" 2>&1 \
+      julia "'"$SCRIPT"'" "$1" >"$LOGFILE" 2>&1 \
         || echo "Julia failed on row $1" >>"$LOGFILE"
     ' _ {}
