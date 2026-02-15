62612ad7f1c0a80738dee6e5b5cc88d1c4a23d0f
Edward Lavender <e.lavender@napier.ac.uk>
2026-02-15 17:17:59 +0000

    Update sim-data.R to use sim-data.jl

--- R/014-sim-data.R
+++ R/014-sim-data.R
@@ -12,7 +12,6 @@
 ###########################
 ###########################
 #### Set up 
-
 #### Wipe workspace 
 rm(list = ls())
 
@@ -21,148 +20,154 @@ library(data.table)
 library(dtplyr)
 library(dplyr, warn.conflicts = FALSE)
 library(JuliaCall)
+library(ggplot2)
 library(patter)
 library(proj.verse)
 library(tictoc)
 files_source_r(here_src())
 
 #### Load data
-fish            <- qs::qread(here_input("fish.qs"))
-moorings        <- qs::qread(here_input_sim("main", "moorings-xy.qs"))
+map             <- terra::rast(here_input("map.tif"))
+champlain_utm   <- qs::qread(here_input("champlain-utm.qs")) |> sf::st_geometry()
 pars_model_move <- qs::qread(here_input("pars-model-move-best.qs"))
 pars_model_obs  <- qs::qread(here_input("pars-model-obs-best.qs"))
+moorings        <- qs::qread(here_input_sim("main", "moorings-xy.qs"))
+xinit           <- fread(here_data_raw_mf("champlain_patter_startLocations.csv"))
+
+#### Set local variables
+set.seed(123L)
 
 
 ###########################
 ###########################
 #### Simulate data 
 
-#### Setup Julia
-julia_connect()
-set_seed()
-julia_source(file.path("Julia", "src", "observation-model.jl"))
-set_map(here_input("map.tif"))
-
 #### Define n_sim
-n_sim <- 30L
+n_sim <- 100L 
+
+#### Define simulation settings
+iter <- 
+  data.table(file_timeline = here_input_sim("main", "timeline.feather"), 
+             file_xinit    = here_input_sim("main", "xinit.feather"),
+             file_moorings = here_input_sim("main", "moorings.feather"), 
+             file_paths_raw = here_input_sim("main", "paths-raw.feather"), 
+             file_acoustics_raw = here_input_sim("main", "acoustics-raw.feather")) |> 
+  cbind(as.data.table(pars_model_move)) |> 
+  cbind(as.data.table(pars_model_obs))
+arrow::write_feather(iter, here_input_sim("main", "iter.feather"))
 
 #### Define timeline (one-month)
 timeline <- seq(as.POSIXct("2025-01-01 00:00:00", tz = "UTC"), 
                 as.POSIXct("2025-01-31 23:58:00", tz = "UTC"), 
                 by = "2 mins")
+timeline <- data.table(timestamp = timeline)
+arrow::write_feather(timeline,  iter$file_timeline)
 
-#### Define movement model
-model_move <- model_move_trout(pars_model_move)
-plot(model_move)
-
-#### Define observation model
-# * We consider the average receiver positions in each StationName
-#   following Futia et al. (2024). 
-# * This is a better representation of the study design than 
-#   assuming all 153 rows in moorings represent available receivers.
-# * We update receiver_start and receiver_end for the simulation timeline. 
-stopifnot(nrow(moorings) == 31L)
-moorings[, receiver_start := min(timeline) -  24 * 60 * 60]
-moorings[, receiver_end := max(timeline) + 24 * 60 * 60]
-model_obs <- model_obs_champlain(moorings, pars_model_obs)
-plot(model_obs)
-
-#### Simulate trajectories and observations (~179 s)
-# This is implemented iteratively to generate exactly n_sim time series for modelling
-# I.e., that pass filter_detections() criteria 
-tic()
-count              <- 1L
-total              <- 1L
-paths_by_path      <- list()
-acoustics_by_path  <- list()
-detections_by_path <- list()
-while (count <= n_sim & total < 100L) {
-  
-  cli::cat_rule()
-  print(paste(count, ", ", total))
-  
-  #### Define tagging locations
-  # (Extract map_value via Patter for linux handling)
-  xinit <- fish[sample.int(1L, replace = TRUE), ]
-  julia_assign("x0", xinit$x)
-  julia_assign("y0", xinit$y)
-  xinit[, map_value := julia_eval('[Patter.extract(env, x0[i], y0[i]) for i in eachindex(x0)]')]
-  xinit <- model_move_xinit(.xinit = xinit, .n_particle = NULL)
-  stopifnot(all(xinit$map_value == 1L))
-  
-  #### Simulate movement paths (~1.6 s)
-  # This returns a data.table with trajectories
-  tic()
-  path <- sim_path_walk(.timeline   = timeline, 
-                        .state      = state_trout(), 
-                        .model_move = model_move,
-                        .xinit      = xinit,
-                        .n_path     = 1L)
-  path[, path_id := count]
-  toc()
-  # Validate that each path starts with the simulated xinit
-  stopifnot(dplyr::all_equal(
-    xinit[, .(x, y, heading)],
-    path |> 
-      group_by(path_id) |> 
-      slice(1L) |> 
-      ungroup() |>
-      select("x", "y", "heading") |> 
-      as.data.table()
-  ))
-  
-  #### Collate capture/recapture locations
-  # xinit <- 
-  #   path |> 
-  #   group_by(path_id) |> 
-  #   slice(c(1, n())) |> 
-  #   select(path_id, timestep, map_value, x, y) |> 
-  #   as.data.table()
-  
-  #### Simulate acoustic observations (~5.5 s)
-  tic()
-  acoustics <- sim_observations(.timeline = timeline, 
-                                .model_obs = model_obs)
-  acoustics <- acoustics$ModelObsAcousticLogisTruncLos[[1]]
-  toc()
-  
-  #### Collate detections data.table (to match real-world data structure)
-  detections <- 
-    acoustics |> 
-    filter(obs == 1L) |> 
-    mutate(individual_id = count, time_id = lubridate::floor_date(timestamp, "months")) |> 
-    select(individual_id, time_id, timestamp, receiver_id = sensor_id, 
-           receiver_x, receiver_y, receiver_alpha, receiver_beta, receiver_gamma) |> 
+#### Simulate initial locations
+# (1) Sample starting points as in Futia et al. (2024)
+# Define starting points
+if (FALSE) {
+  xinit <- 
+    cbind(xinit$start_lon, xinit$start_lat) |> 
+    terra::vect(crs = "WGS84") |> 
+    terra::project(terra::crs(map)) |> 
+    terra::geom() |> 
+    as.data.table() |> 
+    select("x", "y") 
+  # Resample 
+  xinit <- 
+    xinit[sample.int(.N, size = n_sim, replace = TRUE)] |> 
+    mutate(map_value = terra::extract(map, cbind(x, y))$map_value, 
+           heading = runif(n(), 0, 2 * pi)) |> 
+    select("map_value", "x", "y", "heading") |>
     as.data.table()
-  
-  #### Filter detections
-  n <- nrow(filter_detections(detections))
-  
-  #### Record outputs, if successful
-  if (n > 0L) {
-    paths_by_path[[count]]      <- copy(path)
-    acoustics_by_path[[count]]  <- copy(acoustics)
-    detections_by_path[[count]] <- copy(detections)
-    count <- count + 1L
-  }
-  total <- total + 1L
-  
 }
-toc()
+# (2) Sample starting points uniformaly across study area 
+if (TRUE) {
+  xinit <-
+    map |>
+    terra::spatSample(size = 100L, method = "random",
+                      replace = FALSE, na.rm = TRUE,
+                      xy = TRUE) |>
+    mutate(heading = runif(n(), 0, 2 * pi)) |>
+    select("map_value", "x", "y", "heading") |>
+    as.data.table()
+  stopifnot(nrow(xinit) == 100L)
+}
+stopifnot(!all(is.na(xinit$map_value)))
+arrow::write_feather(xinit, iter$file_xinit)
+terra::plot(map)
+points(xinit$x, xinit$y)
+# text(xinit$x, xinit$y, 1:nrow(xinit))
 
-#### Collate datasets
-# Check lists
-stopifnot(length(paths_by_path) == n_sim)
-stopifnot(length(acoustics_by_path) == n_sim)
-stopifnot(length(detections_by_path) == n_sim)
-# Collate data
-paths      <- rbindlist(paths_by_path)
-detections <- rbindlist(detections_by_path)
-# Additional checks
+#### Define model_move
+# The settings are defined above
+
+#### Define model_obs_pars
+moorings <-
+  moorings |> 
+  cbind(as.data.table(pars_model_obs)) |> 
+  # mutate(receiver_start = lubridate::floor_date(min(timeline$timestamp)), 
+  #        receiver_end = lubridate::floor_date(max(timeline$timestamp))) |>
+  select(sensor_id = "receiver_id", 
+         "receiver_x", "receiver_y", 
+         "receiver_alpha", "receiver_beta", "receiver_gamma") |> 
+  as.data.table()
+arrow::write_feather(moorings, iter$file_moorings)
+
+#### Simulate observations (~500 s)
+# Run 001-sim-data.jl 
+if (!file.exists(iter$file_acoustics_raw)) {
+  tic()
+  julia <- Sys.which("julia")
+  stopifnot(nzchar(julia))
+  system2("/Users/lavended/.juliaup/bin/julia",
+          args = "./Julia/001-sim-data.jl",
+          env = c(LD_LIBRARY_PATH = ""),
+          stdout = TRUE,
+          stderr = TRUE)
+  toc()
+}
+
+#### Collate outputs
+paths     <- arrow::read_feather(iter$file_paths_raw)
+acoustics <- arrow::read_feather(iter$file_acoustics_raw)
+
+#### Process datasets
+# Process paths
+head(paths)
+# Process acoustics
+acoustics <- 
+  acoustics |> 
+  select("path_id", "timestamp", 
+         "obs", "sensor_id", 
+         "receiver_x", "receiver_y", 
+         "receiver_alpha", "receiver_beta", "receiver_gamma") |> 
+  as.data.table()
+# Process detections data.table (to match real-world data structure)
+detections <- 
+  acoustics |> 
+  filter(obs == 1L) |> 
+  mutate(individual_id = path_id) |> 
+  select("individual_id", "timestamp", receiver_id = "sensor_id", 
+         "receiver_x", "receiver_y", "receiver_alpha", "receiver_beta", "receiver_gamma") |> 
+  as.data.table()
+
+#### Validate datasets
+# Check path_ids 
 stopifnot(identical(unique(paths$path_id), 1:n_sim))
-stopifnot(identical(unique(detections$individual_id), 1:n_sim))
-
-#### Validation
+stopifnot(identical(unique(acoustics$path_id), 1:n_sim))
+# stopifnot(identical(unique(detections$individual_id), 1:n_sim))
+# Validate that each path starts with the simulated xinit
+stopifnot(dplyr::all_equal(
+  xinit[, .(x, y, heading)],
+  paths |> 
+    group_by(path_id) |> 
+    slice(1L) |> 
+    ungroup() |>
+    select("x", "y", "heading") |> 
+    as.data.table()
+))
 # We should only record detections within receiver_gamma of receiver
 positions <- 
   paths |> 
@@ -173,6 +178,47 @@ positions <-
   as.data.table()
 stopifnot(all(positions$dist <= positions$receiver_gamma))
 
+#### Examine simulated datasets
+# Examine paths 
+head(paths)
+# Plot paths (12 mins)
+if (FALSE) {
+  tic()
+  png(here_fig("sim", "main", "qplot-paths.png"), 
+      height = 20, width = 20, units = "in", res = 800)
+  p <- 
+    ggplot() +
+    geom_sf(data = champlain_utm) + 
+    geom_path(aes(x, y, colour = timestep), data = paths, linewidth = 0.2) + 
+    facet_wrap(~path_id, nrow = 20, ncol = 5) 
+  print(p)
+  dev.off()
+  toc()
+}
+# Review the number of detections per individual
+acoustics |> 
+  group_by(path_id) |> 
+  summarise(n_detections = length(which(obs == 1L))) |> 
+  ungroup() |>
+  summarise(utils.add::basic_stats(n_detections))
+# Review number of individuals with zero detections
+acoustics |> 
+  group_by(path_id) |> 
+  summarise(zero_detections = all(obs == 0L)) |> 
+  filter(zero_detections)
+# Plot detections (~5 s)
+tic()
+png(here_fig("sim", "main", "qplot-detections.png"), 
+    height = 20, width = 20, units = "in", res = 800)
+p <- 
+  acoustics[obs == 1L, ] |> 
+  ggplot() + 
+  geom_point(aes(timestamp, path_id)) + 
+  theme_bw()
+print(p)
+dev.off()
+toc()
+
 
 ###########################
 ###########################
@@ -181,7 +227,7 @@ stopifnot(all(positions$dist <= positions$receiver_gamma))
 qs::qsave(timeline, here_input_sim("main", "timeline.qs"))
 qs::qsave(paths, here_input_sim("main", "paths.qs"))
 qs::qsave(moorings, here_input_sim("main", "moorings.qs"))
-qs::qsave(acoustics_by_path, here_input_sim("main", "acoustics-by-path.qs"))
+qs::qsave(acoustics, here_input_sim("main", "acoustics.qs"))
 qs::qsave(detections, here_input_sim("main", "detections.qs"))
 
 
