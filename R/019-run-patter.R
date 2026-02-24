40a68d208fc05ea606381c87ece41d0e77ea8b87
edwardlavender <edlavender01@gmail.com>
2026-02-24 18:54:47 +0100

    Revise run-patter.R script

--- R/019-run-patter.R
+++ R/019-run-patter.R
@@ -1,20 +1,16 @@
 ###########################
 ###########################
-#### run-patter.R
+#### synthesise-patter.R
 
 #### Aims
-# 1) This script provides a generic workflow for running particle algorithms 
+# 1) This script collates particle outputs into occupancy maps and residency estimates
+# - Particles are produced in batches by block
+# - We are interested in chain-level (seasonal) patterns (each chain comprises multiple blocks)
+# - Here, we collate outputs for each chain into a map and residency estimates
 
 #### Prerequisites
 # 1) Generate particle algorithm outputs via run-algorithms.jl
 # 2) This code should be run on the same machine (where the particle outputs live)
-
-# TO DO REVISE THIS CODE
-# SPLIT by chain_id & for each chain
-# - loop over blocks
-# - read in all pou files 
-# - filter by chain time series ENSURING WE ONLY KEEP ONE FILE!
-# - define output files
 
 
 ###########################
@@ -33,11 +29,12 @@ library(data.table)
 library(dtplyr)
 library(dplyr, warn.conflicts = FALSE)
 library(proj.verse)
+library(spatial.extensions)
 library(tictoc)
 files_source_r(here_src())
 
 #### Load data
-# NA
+map <- terra::rast("./data/input/map.tif")
 
 
 ###########################
@@ -56,38 +53,61 @@ iteration           <- qs::qread(here_input_analysis("iteration.qs"))
 
 ###########################
 ###########################
-#### Run patter
+#### Synthesise outputs
 
-#### (1) Run Julia workflow
-# Run 001-run-algorithms.jl via tmux/bash
-
-#### (2) Run R workflows (mapping)
-
+#### Examine timings & folder sizes
+#
 ## For "sim" iteration[1, ]:
 # * Memory required per iteration : 251.46 MB 
 # * Time required per iteration   : 1.012 s
 # * ETA for 210 iteration         : 3.5 mins on 1 cl (1.012 * 210 / 60)
-
+#
 ## For "real" iteration[1, ]:
 # * Memory required per iteration : TO DO
 # * Time required per iteration   : TO DO
 # * ETA for 2723 iteration        : TO DO
 # > We can safely run ≈ TO DO N CPUs
 
-## Subset iterations
-# We will produce maps for all iterations with:
+#### Subset iterations
+# We will produce maps for all chains where _all blocks_ have:
 # a) julia = FALSE
-# b) julia = TRUE with pou-{i}.feather files
+# b) julia = TRUE with {n_batch} pou-{i}.feather files
 #    (these were produced when both forward and backward filters were run successfully)
+# i.e., We drop any chains in which one or more blocks failed
+iteration[, chain_group := paste(individual_id, chain_id, sensitivity)]
 iteration[, julia_success := sapply(iteration$folder_output_block, function(folder) {
-  length(list.files(folder, "pou-")) > 0L
+  length(list.files(folder, "pou-")) == iteration$n_batch[1]
 })]
 iteration[julia == FALSE, julia_success := as.logical(NA)]
 table(iteration$julia_success[iteration$julia])
-iteration <- iteration[!julia | (julia & julia_success), ]
+iteration <-
+  iteration |>
+  group_by(chain_group) |> 
+  filter(all(!julia | (julia & julia_success))) |> 
+  as.data.table()
 stopifnot(nrow(iteration) > 0L)
 
-## Define cluster
+#### Pre-compute blank (starting) occupancy map
+# For each chain, this is a starting point that is updated block-by-block
+occupancy_zero <- terra::setValues(map, 0)
+occupancy_zero <- terra::mask(occupancy_zero, map)
+terra::writeRaster(occupancy_zero, 
+                   here_input("occupancy-zero.tif"), 
+                   overwrite = TRUE)
+# terra::plot(occupancy_zero)
+
+#### Pre-compute uniform map
+# We assume uniform distribution over the study area when julia = FALSE
+# (I.e., a long time after the last detection)
+occupancy_uniform <- terra::setValues(map, 1)
+occupancy_uniform <- terra::mask(occupancy_uniform, map)
+occupancy_uniform <- spatNormalise(occupancy_uniform)
+terra::writeRaster(occupancy_uniform, 
+                   here_input("occupancy-uniform.tif"), 
+                   overwrite = TRUE)
+# terra::plot(occupancy_uniform)
+
+#### Define cluster
 tic()
 cl <- parallel::makeCluster(5L)
 parallel::clusterEvalQ(cl, {
@@ -97,83 +117,94 @@ parallel::clusterEvalQ(cl, {
 })
 toc()
 
-## Make maps
-tic()
-cl_lapply(split(iteration, seq_len(nrow(iteration))), 
-          .cl = cl,
-          .fun = function(it) {
+#### Synthesise outputs
+# For each chain, compute file_occupancy and file_residency
+toc()
+cl_lapply(
+  split(iteration, iteration$chain_group), 
+  .cl = cl,
+  .fun = function(chain) {
   
-  # Read data
-  # tic()
-  # it <- iteration[1, ]
-  map         <- terra::rast("./data/input/map.tif")
-  regions     <- terra::rast("./data/input/regions.tif")
-  it_block    <- seq(it$time_start, it$time_end, by = "2 mins")
-  it_timeline <- arrow::read_feather(it$file_timeline)
-  it_timeline <- it_timeline[!is.na(block), ]
+  #### Read datasets
+  # chain           <- split(iteration, iteration$chain_group)[[1]]
+  map               <- terra::rast("./data/input/map.tif")
+  regions           <- terra::rast("./data/input/regions.tif")
+  occupancy         <- terra::rast("./data/input/occupancy-zero.tif")
+  occupancy_uniform <- terra::rast("./data/input/occupancy-uniform.tif")
   
-  if (it$julia) {
+  #### Update map for each block in the chain 
+  # This is implemented iteratively to keep memory use down
+  for (i in seq_len(nrow(chain))) {
     
-    # Compute a data.table of POU (x, y, mark = probability mass)
-    coord <- 
-      # List files 
-      # (convergence failures handled above)
-      list.files(it$folder_output_block, full.names = TRUE, pattern = "pou-") |> 
-      lapply(arrow::read_feather) |>
-      rbindlist() |> 
-      arrange(timestep, x, y) |> 
-      # Filter time series by relevant period
-      # TO DO 
-      filter(timestep %in% it_timeline$timestep)
-      group_by(x, y) |> 
-      summarise(mark = sum(mark)) |> 
-      ungroup() |> 
-      mutate(mark = mark / nrow(it_timeline)) |> 
-      as.data.table()
+    # Define iteration 
+    it             <- chain[i, ]
     
-    # Verify that weights sum to one 
-    stopifnot(isTRUE(all.equal(1, sum(coord$mark))))
+    # Define timeline 
+    # * it_timeline is the modelled timeline for a block (may be longer than the block)
+    # * block_timeline is timeline of interest 
+    # * Each chain is made up of several non-overlapping block_timeline(s) 
+    it_timeline    <- arrow::read_feather(it$file_timeline)
+    block_timeline <- seq(it$block_start, it$block_end, by = "2 mins")
     
-    # Map occupancy 
-    occupancy <- terra::rasterize(coord, map, values = coord$mark)
+    # Update occupancy map for block 
+    if (it$julia) {
+      
+      # Compute the total weight per cell (cell, mark) over all time steps
+      marks <- 
+        # Load all POU files
+        list.files(it$folder_output_block, full.names = TRUE, pattern = "pou-") |> 
+        lapply(arrow::read_feather) |>
+        rbindlist() |> 
+        lazy_dt(immutable = FALSE) |>
+        # Assign timestamps and filter by block_timeline
+        mutate(timestamp = it_timeline$timestamp[match(timestep, it_timeline$timestep)]) |> 
+        filter(timestamp %in% block_timeline) |> 
+        # For each cell, compute the overall weight 
+        mutate(cell = terra::cellFromXY(map, cbind(x, y))) |> 
+        group_by(cell) |> 
+        summarise(mark = sum(mark)) |> 
+        as.data.table()
+      
+      # Update map
+      occupancy[marks$cell] <- occupancy[marks$cell] + marks$mark
+        
+    } else {
+      
+      # For un-modelled blocks, assume uniform weights & update map for all time steps
+      occupancy <- occupancy + (uniform_occupancy * length(block_timeline))
     
-  } else {
-    
-    # For time blocks with julia = FALSE, we use a uniform map
-    occupancy <- terra::setValues(map, 1)
-    occupancy <- terra::mask(occupancy, map)
-    occupancy <- spatNormalise(occupancy)
+    }
     
   }
   
+  #### Process map
   occupancy <- terra::classify(occupancy, cbind(NA, 0))
   occupancy <- terra::mask(occupancy, map)
+  occupancy <- spatNormalise(occupancy)
   names(occupancy) <- "map_value"
   # terra::plot(occupancy)
   
-  # Estimate residency
+  #### Estimate residency by region
   residency <- 
     occupancy |> 
     terra::zonal(regions, fun = "sum", na.rm = TRUE) |> 
     lazy_dt() |> 
     mutate(unit_id           = it$unit_id,
            individual_id     = it$individual_id,
-           time_id           = it$time_id,
+           chain_id          = chain$chain_id[1],
            sensitivity       = it$sensitivity,
            sensitivity_label = it$sensitivity_label) |>
-    select("unit_id", "individual_id", "time_id", "sensitivity", "sensitivity_label",
+    select("unit_id", "individual_id", "chain_id", "sensitivity", "sensitivity_label",
            region = "map_value", estimate = "map_value.1") |>
     as.data.table()
-
-  # Write to file
-  qs::qsave(residency, it$file_residency)
-  terra::writeRaster(occupancy, it$file_occupancy, overwrite = TRUE)
   
-  # (optional) Cleanup pou-{i}.feather files
+  #### Write to file
+  terra::writeRaster(occupancy, chain$file_occupancy[1], overwrite = TRUE)
+  qs::qsave(residency, chain$file_residency[1])
+
+  #### (optional) Cleanup pou-{i}.feather files
   # unlink(list.files(it$folder_output_block, full.names = TRUE, pattern = "pou-"))
   
-  # lobstr::mem_used()
-  # toc()
   invisible(NULL)
   
 })
