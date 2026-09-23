b52e529aef3a7abb15f0228da34534b620df9eb1
edwardlavender <edlavender01@gmail.com>
2026-09-23 18:12:08 +0200

    Check time zones

--- R/006-setup-data-detection-pr-futia.R
+++ R/006-setup-data-detection-pr-futia.R
@@ -42,6 +42,12 @@ metadata   <- qs::qread(here_data_raw("model-obs","futia-et-al-2025",
 ###########################
 #### Define detections/non-detections
 
+#### Review time zones
+lubridate::tz(detections$detection_timestamp_utc)
+lubridate::tz(detections$detection_timestamp_utc)
+lubridate::tz(metadata$set_dt)
+lubridate::tz(metadata$pull_dt)
+
 #### Merge range test detections with associated metadata
 # The set and pull times are exact to the second that the range test tags were set and deployed, so there should not be any more detections than what is possible in that window. I added the extension to the recover times as some receivers had minor clock drift that was not completely accounted for in the time corrections, so there were some deployments where the detections did not line up perfectly within the recorded deployment period. Adding 75 seconds allowed me to capture all detections but was a short enough interval that it did not overlap with the following deployment. 
 detections <- 
